8aa3f6a0bbd428ed3b2c3b79a8b698e400b8b652
naimuddin94 <developernaimuddin@gmail.com>
2024-12-25 11:01:52 +0600

    skill model create

---
+++ src/interface/index.ts
@@ -0,0 +1,17 @@
+import { ObjectId } from "mongoose";
+
+export interface ISkill {
+  name: string;
+  url: string;
+  image: string;
+}
+
+export interface IProject {
+  title: string;
+  live_link: string;
+  frontend_github: string;
+  backend_github: string;
+  images: string[];
+  description: string;
+  technologies: ObjectId[];
+}
