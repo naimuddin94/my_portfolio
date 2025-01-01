78999f299cb8168e4a007998102844ef9f257286
naimuddin94 <developernaimuddin@gmail.com>
2025-01-01 21:58:45 +0600

    DB connect file create

---
+++ src/lib/connectDB.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) {
+    console.log("Already connected to MongoDB.");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+    isConnected = true;
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.log("There was an error connecting to MongoDB", error);
+    isConnected = false;
+  }
+};
+
+export default connectDB;
