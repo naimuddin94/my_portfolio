155774f237b8ad148e2a9dbcc2afed3cb740baa1
naimuddin94 <developernaimuddin@gmail.com>
2025-01-07 18:03:52 +0600

    feat: create useTypewriter.ts

---
+++ src/hook/useTypewriter.ts
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export function useTypewriter(text: string, speed: number = 50) {
+  const [displayedText, setDisplayedText] = useState("");
+
+  useEffect(() => {
+    let i = 0;
+    const typingInterval = setInterval(() => {
+      if (i < text.length) {
+        setDisplayedText((prev) => prev + text.charAt(i));
+        i++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, speed);
+
+    return () => clearInterval(typingInterval);
+  }, [text, speed]);
+
+  return displayedText;
+}
