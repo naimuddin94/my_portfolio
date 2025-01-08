11c47f0e5e2a2525a96c86503e6639139243c1bb
naimuddin94 <developernaimuddin@gmail.com>
2025-01-08 22:11:20 +0600

    home page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import DeveloperBanner from "@/components/ui/shared/DeveloperBanner";
+import ProjectsSection from "@/components/ui/shared/ProjectsSection";
+import SkillsSection from "@/components/ui/shared/SkillsSection";
 
 export default function Home() {
   return (
     <main>
       <DeveloperBanner />
+      <SkillsSection />
+      <ProjectsSection />
     </main>
   );
 }
